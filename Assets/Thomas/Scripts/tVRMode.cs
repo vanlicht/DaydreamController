016b576a7338b50536127f92aeb84f5f71e84392
Thomas Huang <vanlicht@gmail.com>
2017-01-03 17:42:36 -0800

    teleport function; work on tilt brush ui

---
+++ Assets/Thomas/Scripts/tVRMode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class tVRMode : MonoBehaviour
+{
+    bool VRState = true;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void tVRToggle()
+    {
+        VRState = !VRState;
+        if(VRState == false)
+        {
+            VRSettings.enabled = false;
+        }
+        
+    }
+}
