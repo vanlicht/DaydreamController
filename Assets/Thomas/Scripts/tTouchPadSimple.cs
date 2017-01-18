dcf43707442b683eed1e57963d0e1547fa698771
vanlicht <vanlicht@gmail.com>
2017-01-18 10:37:36 -0800

    start looking into controller touch pad

---
+++ Assets/Thomas/Scripts/tTouchPadSimple.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tTouchPadSimple : MonoBehaviour
+{
+    #region Private Fields
+    bool isScrolling;
+    bool isTrackingTouches;
+    #endregion
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!isScrolling && GvrController.IsTouching)
+        {
+            //Start tracking
+        }
+        
+        //Debug.Log(GvrController.IsTouching);
+        //Debug.Log(GvrController.TouchPos);
+	}
+}
